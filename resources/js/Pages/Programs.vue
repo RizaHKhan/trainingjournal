6df99c754e5774cfdb74088f9e9c2ad670f4a329
Riza Khan <khanriza@gmail.com>
2022-10-08 14:59:20 -0400

    Removing slot item

--- resources/js/Pages/Programs.vue
+++ resources/js/Pages/Programs.vue
@@ -1,20 +1,11 @@
 <template>
     <Head title="Dashboard" />
-    <AuthenticatedLayout>
-        <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Welcome
-            </h2>
-        </template>
-
+    <AuthenticatedLayout header="Programs">
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 bg-white border-b border-gray-200">
-                        Program list goes here
-                        <pre>
-                          {{ programs }}
-                        </pre>
+                        <Table :rows="programs" cols="programsHeader" />
                     </div>
                 </div>
             </div>
