067943f182531c56e611a34334f2e4b93396740b
Riza Khan <khanriza@gmail.com>
2022-10-08 19:38:46 -0400

    Getting inputs to work for table

--- resources/js/Pages/Workout.vue
+++ resources/js/Pages/Workout.vue
@@ -6,8 +6,25 @@
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 bg-white border-b border-gray-200">
                         <p class="text-h3">{{ date }}</p>
-                        <Table :rows="exercises" cols="workoutHeader" />
-                        {{ exercises }}
+                        <Table
+                            :rows="exercises"
+                            cols="workoutHeader"
+                            :hide-pagination="true"
+                        >
+                            <template #body-cell-weight="{ row }">
+                                <q-td>
+                                    <TextInput v-model="row.weight" />
+                                </q-td>
+                            </template>
+                            <template #body-cell-rpe="{ row }">
+                                <q-td>
+                                    <TextInput v-model="row.rpe" />
+                                </q-td>
+                            </template>
+                        </Table>
+                        <Row justify="end">
+                            <Button label="Save" />
+                        </Row>
                     </div>
                 </div>
             </div>
