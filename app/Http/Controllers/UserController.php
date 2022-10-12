5e46d38b8f22f36a79c251a9f0299424a554648e
riza-khan <riza.khan@appnovation.com>
2022-10-11 21:55:48 -0400

    Update method for user settings

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UpdateUserRequest;
 use Inertia\Inertia;
 
 use Inertia\Response;
@@ -10,6 +11,15 @@ class UserController extends Controller
 {
     public function index(): Response
     {
-        return Inertia::render('Settings');
+        return Inertia::render('Private/Settings');
+    }
+
+    public function update(UpdateUserRequest $request): Response
+    {
+        $validated = $request->validated();
+
+        $request->user()->update($validated);
+
+        return Inertia::render('Private/Settings');
     }
 }
